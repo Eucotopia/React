0060113cde6bc3aaf6a180ce2dd27ba87ddbf187
liwei <wli@pvetec.com>
2023-12-19 10:55:30 +0800

    Writing Markup with JSX

---
+++ office-01/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/**
+ * 1. 返回单个根组件<></>
+ * 2. 标签一定要关闭
+ * 3. React 区分组件和标签；大写开头为组件，小写开头为标签
+ * @constructor
+ */
+export default function Gallery() {
+    return (
+        <>
+            <img
+                src={""}
+                alt={""}/>
+        </>
+    );
+}
+
